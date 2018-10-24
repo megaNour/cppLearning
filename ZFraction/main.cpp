b83f80df3cad7b28c6e9862baa83be2bc24421d7
Unknown <alkotobnour@gmail.com>
2018-10-24 23:56:20 +0200

    operators overloading

--- ZFraction/main.cpp
+++ ZFraction/main.cpp
@@ -6,14 +6,17 @@ int main()
 {
     ZFraction frac1(1,2), frac2(1,2);
     ZFraction frac3(frac1 + frac2);
+    ZFraction frac4(frac1);
+    frac4+=frac3;
 
-    cout << "coucou5 " << frac3.toString() << endl;
-    cout << "coucou5 " << frac3.toString() << endl;
-    cout << to_string(frac1.getValue()) << "\n"
+    cout << frac1.getValue() << "\n"
     << to_string(frac2.getValue()) << "\n"
     << endl;
     if(frac1 == frac2)
+    {
         cout << frac1 << " est egal a " << frac2 << " leur addition donne : " << frac3 << endl;
+    }
+    cout << frac1 << " + " << frac3 << " leur addition donne : " << frac4 << endl;
     cout << "Hello world!" << endl;
     return 0;
 }
--- ZFraction/src/ZFraction.cpp
+++ ZFraction/src/ZFraction.cpp
@@ -1,9 +1,9 @@
 #include "ZFraction.h"
 #include <iostream>
 
-ZFraction::ZFraction(): num(0), den(0){}
+ZFraction::ZFraction(): num(0), den(0) {}
 
-ZFraction::ZFraction(int numerateur, int denominateur): num(numerateur), den(denominateur){}
+ZFraction::ZFraction(int numerateur, int denominateur): num(numerateur), den(denominateur) {}
 
 ZFraction::~ZFraction()
 {
@@ -15,12 +15,12 @@ double ZFraction::getValue() const
     return (double)num/den;
 }
 
-void ZFraction::operator+=(ZFraction const& other)
+ZFraction& ZFraction::operator+=(ZFraction const& other)
 {
-    num = num * other.den;
-    num+= other.num*den;
+    num = num * other.den + other.num*den;
     den*=other.den;
     simplify();
+    return *this;
 }
 
 void ZFraction::simplify()
@@ -32,31 +32,12 @@ void ZFraction::simplify()
         const int t = b;
         b = a%b;
         a=t;
-        std::cout << "a: " << std::to_string(a) << "b: " << std::to_string(b) << "t: " << std::to_string(t) << std::endl;
     }
     num/=a;
     den/=a;
-    std::cout << "num: " << std::to_string(num) << "den: " << std::to_string(den) << std::endl;
-    std::cout << toString() << std::endl;
-
 }
 
-
-ZFraction* ZFraction::add(ZFraction const& fraction2) const
-{
-    int num = (this->num*fraction2.den) + (fraction2.num*this->den);
-    int den = (this->den*fraction2.den);
-    ZFraction resultat(num, den);
-    resultat.simplify();
-    std::cout << "coucou3 " << resultat << std::endl;
-    std::cout << "coucou3.6 " << resultat << std::endl;
-    ZFraction* result(&resultat);
-    std::cout << "coucou3.7 " << result << std::endl;
-    std::cout << "coucou3.8 " << &resultat << std::endl;
-    return result;
-}
-
-ZFraction& ZFraction::substract(ZFraction const& fraction2) const
+ZFraction ZFraction::substract(ZFraction const& fraction2) const
 {
     int num = (this->num*fraction2.den) - (fraction2.num*this->den);
     int den = (this->den*fraction2.den);
@@ -65,7 +46,8 @@ ZFraction& ZFraction::substract(ZFraction const& fraction2) const
     return resultat;
 }
 
-bool operator==(ZFraction const& fraction1, ZFraction const& fraction2){
+bool operator==(ZFraction const& fraction1, ZFraction const& fraction2)
+{
     return fraction1.getValue() == fraction2.getValue();
 }
 
@@ -74,26 +56,17 @@ bool operator!=(ZFraction const& fraction1, ZFraction const& fraction2)
     return !(fraction1==fraction2);
 }
 
-ZFraction& operator+(ZFraction const& fraction1, ZFraction const& fraction2)
+ZFraction operator+(ZFraction const& fraction1, ZFraction const& fraction2)
 {
-    std::cout << "coucou2" << std::endl;
-    ZFraction* resultat = fraction1.add(fraction2);
-    ZFraction &result(*resultat);
-    std::cout << "coucou4 " << resultat->toString() << " " << resultat->getValue() << std::endl;
-    return result;
+    ZFraction resultat = fraction1;
+    resultat+=fraction2;
+    return resultat;
 }
-
-ZFraction& operator-(ZFraction const& fraction1, ZFraction const& fraction2)
-{
-    return fraction1.substract(fraction2);
-}
-
 
 ZFraction operator<(ZFraction const& fraction1, ZFraction const& fraction2)
 {
     return fraction1.getValue() < fraction2.getValue();
 }
-
 
 std::string ZFraction::toString() const
 {
